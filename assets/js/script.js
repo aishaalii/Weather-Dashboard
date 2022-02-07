670efa274818bb4a63cc67cdefc2982fa1a477f4
aishaalii <92413191+aishaalii@users.noreply.github.com>
2022-02-06 23:41:55 -0600

    initail

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var searchBtn= document.querySelector("")
+var City = document.querySelector("")
+var  = document.querySelector("")
+var searchCity = document.querySelector("")
+
+function deleteCity () {
+    localStorage.removeItem("Cities");
+    location.reload();
+}
+
+
+
+
+)
